fb887a05ea70ef98b4c9493f4af2a8d1de817ad6
NevenaGazivoda <nevena.g@hotmail.com>
2021-06-08 20:22:50 +0200

    Dodat Student model

---
+++ FakultetAPI/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FakultetAPI.Models
+{
+    public class Student
+    {
+        public int PKStudentId { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public int BrIndexa { get; set; }
+    }
+}
